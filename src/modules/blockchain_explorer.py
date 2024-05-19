894c4bbe8b44b4b5683ece90b0216869ee050001
Sergen Guelcelik <sergen.guelcelik@rwth-aachen.de>
2024-05-19 22:47:10 +0200

    get withdrawals from bc

--- src/modules/blockchain_explorer.py
+++ src/modules/blockchain_explorer.py
@@ -44,7 +44,9 @@ class BlockchainExplorer:
         if isinstance(addr_list, str):
             addr_list = [addr_list]
         for addr in tqdm(addr_list, desc="Searching blockchain", total=len(addr_list)):
+            tqdm.write("#######################################################")
             self._search(addr)
+            tqdm.write("#######################################################\n")
         with open(os.path.join(self.cache_path, BLACKLIST_FILE), "w") as file:
             for a in self.blacklist:
                 file.write(a + "\n")
@@ -54,28 +56,34 @@ class BlockchainExplorer:
         return list(self.wallets)
         
     def _search(self, addr: str):
-        print(f"Address {addr}:")
+        tqdm.write(f"Address {addr}:")
         if (addr.lower() in self.wallets) or (addr.lower() in self.blacklist):
-            print(f"Skip")
+            tqdm.write(f"Known address, skip...")
             return
 
-        print(f"Get transactions")
+        tqdm.write(f"Get transactions")
         res, status = self._get_api_response(addr)
         if int(status) == 0:
-            print(res)
+            if res:
+                tqdm.write(res)
             return
         if len(res) > 100:  # Addresses with many transactions are blacklisted
-            print(f"Add to blacklist")
+            tqdm.write(f"Add to blacklist")
             self.blacklist.add(addr)
             return
-        print(f"Add to wallets")
+        tqdm.write(f"Add to wallets")
         self.wallets.add(addr)
 
         for elem in res:
             self._search(elem["from"])
             self._search(elem["to"])
 
-    def get_transactions_for_address_list(self, addr_list: List[str], columns: List[str]) -> pd.DataFrame:
+    def get_transactions_and_withdrawals_for_address_list(
+        self,
+        addr_list: List[str],
+        columns: List[str],
+        columns_w: List[str]
+    ) -> Tuple[pd.DataFrame, pd.DataFrame]:
         """
         Iterates over addr_list and looks for swaps and puts them in a df.
         TODO: More chains (currently only Ethereum)
@@ -83,55 +91,100 @@ class BlockchainExplorer:
         if isinstance(addr_list, str):
             addr_list = [addr_list]
 
-        df = pd.DataFrame(columns=columns)
+        df_tx = pd.DataFrame(columns=columns)
+        df_w = pd.DataFrame(columns=columns_w)
         for addr in tqdm(addr_list, desc="Collecting transactions", total=len(addr_list)):
-            tx_df = self.get_transactions_for_address(addr, columns)
-            if tx_df.empty:
-                continue
-            if df.empty:
-                df = tx_df
-            else:
-                df = pd.concat((df, tx_df))
+            df_tx_a, df_w_a = self.get_transactions_and_withdrawals_for_address(addr, columns, columns_w)
+            if not df_tx_a.empty:
+                if df_tx.empty:
+                    df_tx = df_tx_a
+                else:
+                    df_tx = pd.concat((df_tx, df_tx_a))
+            if not df_w_a.empty:
+                if df_w.empty:
+                    df_w = df_w_a
+                else:
+                    df_w = pd.concat((df_w, df_w_a))
 
-        return df
+        return df_tx, df_w
 
-    def get_transactions_for_address(self, addr: str, columns: List[str]) -> pd.DataFrame:
+    def get_transactions_and_withdrawals_for_address(
+        self,
+        addr: str,
+        columns: List[str],
+        columns_w: List[str]
+    ) -> Tuple[pd.DataFrame, pd.DataFrame]:
         res, _ = self._get_api_response(addr)
         grouped_tx = self._get_grouped_txs(res)
-        df = pd.DataFrame(columns=columns)
+        df_tx_a = pd.DataFrame(columns=columns)
+        df_w_a = pd.DataFrame(columns=columns_w)
         for g in grouped_tx.values():
             if len(g) < 2:
+                if all([a in self.wallets for a in [g[0]["from"], g[0]["to"]]]):
+                    df_w_t = self._process_transfer(g[0], addr)
+                    if df_w_a.empty:
+                        df_w_a = df_w_t
+                    else:
+                        df_w_a = pd.concat((df_w_a, df_w_t))
                 continue
-            def calc_amount(tx: Dict) -> float:
-                dec = int(tx["tokenDecimal"])
-                return float(tx["value"]) / (10**dec)
 
-            dt = datetime.strftime(
-                datetime.fromtimestamp(int(g[0]["timeStamp"])),
-                "%Y-%m-%d %H:%M:%S"
-            )
-            sell_sym = g[0]["tokenSymbol"] if g[0]["from"] == addr else g[1]["tokenSymbol"]
-            buy_sym = g[1]["tokenSymbol"] if g[0]["from"] == addr else g[0]["tokenSymbol"]
-            sell_amount = calc_amount(g[0]) if g[0]["from"] == addr else calc_amount(g[1])
-            buy_amount = calc_amount(g[1]) if g[0]["from"] == addr else calc_amount(g[0])
-            fee_eth = (float(g[0]["gasUsed"]) / 1e9) * (float(g[0]["gasPrice"]) / 1e9)
-            swap = {
-                "Datetime": dt,
-                "Pair": buy_sym + "-" + sell_sym,
-                "Side": "buy",
-                "Size": buy_amount,
-                "Funds": -sell_amount,
-                "Fee": fee_eth,
-                "Fee currency": "ETH",
-                "Broker": "DEX"  # TODO: Get DEX
-            }
-            df_tx = pd.DataFrame([swap])
-            if df.empty:
-                df = df_tx
+            df_tx_s = self._process_swap(g, addr)
+            if df_tx_a.empty:
+                df_tx_a = df_tx_s
             else:
-                df = pd.concat((df, df_tx))
+                df_tx_a = pd.concat((df_tx_a, df_tx_s))
 
-        return df
+        return df_tx_a, df_w_a
+
+    def _get_datetime(self, timestamp: int) -> str:
+        dt = datetime.strftime(
+            datetime.fromtimestamp(timestamp),
+            "%Y-%m-%d %H:%M:%S"
+        )
+        return dt
+
+    def _calc_gas(self, t: Dict) -> float:
+        return (float(t["gasUsed"]) / 1e9) * (float(t["gasPrice"]) / 1e9)
+
+    def _process_transfer(self, t: Dict, addr: str) -> pd.DataFrame:
+        dt = self._get_datetime(int(t["timeStamp"]))
+        fee = self._calc_gas(t)
+
+        transfer = {
+            "Datetime": dt,
+            "Coin": t["tokenSymbol"],
+            "Chain": "ETH",
+            "Address": t["to"],
+            "TxHash": t["hash"],
+            "Fee": fee,
+            "Fee currency": "ETH",
+        }
+
+        return pd.DataFrame([transfer])
+
+    def _process_swap(self, g: List[Dict], addr: str) -> pd.DataFrame:
+        def calc_amount(tx: Dict) -> float:
+            dec = int(tx["tokenDecimal"])
+            return float(tx["value"]) / (10**dec)
+
+        dt = self._get_datetime(int(g[0]["timeStamp"]))
+
+        sell_sym = g[0]["tokenSymbol"] if g[0]["from"] == addr else g[1]["tokenSymbol"]
+        buy_sym = g[1]["tokenSymbol"] if g[0]["from"] == addr else g[0]["tokenSymbol"]
+        sell_amount = calc_amount(g[0]) if g[0]["from"] == addr else calc_amount(g[1])
+        buy_amount = calc_amount(g[1]) if g[0]["from"] == addr else calc_amount(g[0])
+        fee_eth = self._calc_gas(g[0])
+        swap = {
+            "Datetime": dt,
+            "Pair": buy_sym + "-" + sell_sym,
+            "Side": "buy",
+            "Size": buy_amount,
+            "Funds": -sell_amount,
+            "Fee": fee_eth,
+            "Fee currency": "ETH",
+            "Broker": "DEX"  # TODO: Get DEX
+        }
+        return pd.DataFrame([swap])
 
     def _get_grouped_txs(self, txs):
         grouped = defaultdict(list)
